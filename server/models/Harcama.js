f7dededa862d115415587f9834e32d6a4088758f
fetaV <vatef06@gmail.com>
2024-06-08 22:29:56 +0300

    zamanlama eklendi

--- server/models/Harcama.js
+++ server/models/Harcama.js
@@ -2,18 +2,21 @@
 
 const mongoose = require("mongoose")
 
-const MaasSchema = new mongoose.Schema({
-  aciklama: String,
-  kullanim: {
-    type: Number,
-    enum: [0, 1, 2, 3], // 0: İhtiyaç, 1: Yatırım, 2: Lüks
+const MaasSchema = new mongoose.Schema(
+  {
+    aciklama: String,
+    kullanim: {
+      type: Number,
+      enum: [0, 1, 2, 3], // 0: İhtiyaç, 1: Yatırım, 2: Lüks
+    },
+    miktar: Number,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  miktar: Number,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model("Harcama", MaasSchema)
