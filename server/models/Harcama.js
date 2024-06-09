ca5cf13dc5913036968ae0b45e08385e63376744
fetaV <vatef06@gmail.com>
2024-06-09 23:09:45 +0300

    eski

--- server/models/Harcama.js
+++ server/models/Harcama.js
@@ -2,29 +2,21 @@
 
 const mongoose = require("mongoose")
 
-const HarcamaSchema = new mongoose.Schema(
+const MaasSchema = new mongoose.Schema(
   {
     aciklama: String,
     kullanim: {
       type: Number,
-      enum: [0, 1, 2, 3], // 0: İhtiyaç, 1: Yatırım, 2: Lüks, 3: Diğer
+      enum: [0, 1, 2, 3], // 0: İhtiyaç, 1: Yatırım, 2: Lüks
     },
     miktar: Number,
-  },
-  { timestamps: true }
-)
-
-const UserHarcamalarSchema = new mongoose.Schema(
-  {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      unique: true,
     },
-    harcamalar: [HarcamaSchema],
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model("UserHarcamalar", UserHarcamalarSchema)
+module.exports = mongoose.model("Harcama", MaasSchema)
